e7b04810aa31f672451cd77d804669925d388b44
kme-developer <electruc0095@gmail.com>
2024-07-13 16:03:19 +0900

    name. 컨트롤러 파일 이름 수정.

--- src/domains/key-management-service/key.management.module.ts
+++ src/domains/key-management-service/key.management.module.ts
@@ -4,7 +4,7 @@ import { RedisModule } from 'src/providers/redis/redis.module';
 import { LoggerModule } from 'src/providers/logger/logger.module';
 import { RabbitmqModule } from 'src/providers/rabbitmq/rabbitmq.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { KeyManagementV1Controller } from './controllers/action.v1.controller';
+import { KeyManagementV1Controller } from './controllers/key.management.v1.controller';
 import { KeyManagementService } from './key.management.service';
 import { WalletAccount } from './entities/wallet.account.entity';
 import { WalletAccountRepository } from './repositories/wallet.account.repository';
